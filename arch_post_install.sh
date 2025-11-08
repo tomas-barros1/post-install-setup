62ca540651a40deb4d78cfa6017e6b7725593e08
tomas-barros1 <tomasabbarros3@gmail.com>
2025-11-07 23:58:15 -0300

    second

--- arch_post_install.sh
+++ arch_post_install.sh
@@ -8,7 +8,7 @@ sudo pacman -S --noconfirm --needed \
         alacritty qbittorrent neovim fish flatpak fzf unzip zip \
         eza starship base-devel hyprpaper wofi wl-paste cliphist \
         grim slurp hyprsunset hyprpaper yarn python-pip dbeaver docker \
-        docker-compose ddcutil lxappearance
+        docker-compose ddcutil lxappearance meson
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -26,13 +26,20 @@ mv hypr ~/.config
 cd ..
 sudo rm -r dotfiles
 
+git clone --depth=1 https://github.com/realmazharhussain/nautilus-code.git
+cd nautilus-code
+meson setup build
+meson install -C build
+cd ..
+sudo rm -r nautilus-code
+
 curl https://mise.run | sh
 echo '~/.local/bin/mise activate fish | source' >> ~/.config/fish/config.fish
 
 mise use -g ruby@latest
 mise use -g nodejs@latest
 
-yay -S --noconfirm --needed brave catppuccin-gtk-theme-git lazydocker lazygit vdu_controls
+yay -S --noconfirm --needed nautilus-open-any-terminal brave catppuccin-gtk-theme-git lazydocker lazygit vdu_controls
 
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
@@ -46,5 +53,10 @@ omf install zoxide rails fzf
 sudo ufw enable
 sudo ufw allow https
 sudo ufw allow ssh
+
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal keybindings '<Ctrl><Alt>t'
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal new-tab true
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal flatpak system
 
 chsh
